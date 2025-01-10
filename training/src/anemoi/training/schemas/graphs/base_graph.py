12973280e044d92914271abac577100495b1d6ae
chebertpinard <charlie.hebert-pinard@ec.gc.ca>
2025-01-10 14:30:36 +0000

    fix: add missing graph builder schema and description.

--- training/src/anemoi/training/schemas/graphs/base_graph.py
+++ training/src/anemoi/training/schemas/graphs/base_graph.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2024 ECMWF.
+# (C) Copyright 2024-2025 ECMWF.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -9,58 +9,63 @@
 
 from __future__ import annotations
 
-from typing import Literal
+import logging
+from typing import Any
 
 from pydantic import BaseModel
 from pydantic import Field
 from pydantic import field_validator
 
-from .edge_schemas import CutoffEdgeSchema  # noqa: TC001
-from .edge_schemas import EdgeAttributeSchema  # noqa: TC001
-from .edge_schemas import KNNEdgeSchema  # noqa: TC001
-from .edge_schemas import MultiScaleEdgeSchema  # noqa: TC001
-from .node_schemas import IcosahedralandHealPixNodeSchema  # noqa: TC001
-from .node_schemas import LimitedAreaIcosahedralandHealPixNodeSchema  # noqa: TC001
-from .node_schemas import LimitedAreaNPZFileNodesSchema  # noqa: TC001
-from .node_schemas import NPZnodeSchema  # noqa: TC001
-from .node_schemas import ZarrNodeSchema  # noqa: TC001
+from .edge_schemas import EdgeAttributeSchema
+from .edge_schemas import EdgeBuilderSchemas
+from .node_schemas import NodeAttributeSchemas
+from .node_schemas import NodeBuilderSchemas
 
-
-class AreaWeightSchema(BaseModel):
-    target_: str = Field("anemoi.graphs.nodes.attributes.AreaWeights", alias="_target_")
-    norm: Literal["unit-max", "l1", "l2", "unit-sum", "unit-std"] = Field(default="unit-max")
-
-    @field_validator("target_")
-    @classmethod
-    def check_valid_target(cls, target: str) -> str:
-        assert target == "anemoi.graphs.nodes.attributes.AreaWeights"
-        return target
+LOGGER = logging.getLogger(__name__)
 
 
 class NodeSchema(BaseModel):
-    node_builder: (
-        ZarrNodeSchema
-        | NPZnodeSchema
-        | LimitedAreaNPZFileNodesSchema
-        | LimitedAreaNPZFileNodesSchema
-        | IcosahedralandHealPixNodeSchema
-        | LimitedAreaIcosahedralandHealPixNodeSchema
-    )
-    attributes: dict[str, AreaWeightSchema] | None = None
+    node_builder: NodeBuilderSchemas | Any
+    "Node builder schema."
+    attributes: dict[str, NodeAttributeSchemas] | None = None
+    "Dictionary of attributes with names as keys and anemoi.graph.nodes.attributes objects as values."
+
+    @field_validator("node_builder")
+    @classmethod
+    def validate_nodebuilder(cls, node_builder: NodeBuilderSchemas | Any) -> NodeBuilderSchemas | Any:
+        if not isinstance(node_builder, NodeBuilderSchemas):
+            LOGGER.warning("%s not defined in anemoi.", node_builder)
+        return node_builder
 
 
 class EdgeSchema(BaseModel):
-    source_name: str
-    target_name: str
-    edge_builders: list[CutoffEdgeSchema | KNNEdgeSchema | MultiScaleEdgeSchema]
+    source_name: str = Field(examples=["data", "hidden"])
+    "Source of the edges."
+    target_name: str = Field(examples=["data", "hidden"])
+    "Target of the edges."
+    edge_builders: list[EdgeBuilderSchemas | Any]
+    "Edge builder schema."
     attributes: dict[str, EdgeAttributeSchema]
+    "Dictionary of attributes with names as keys and anemoi.graph.edges.attributes objects as values."
+
+    @field_validator("edge_builders")
+    @classmethod
+    def validate_edgebuilders(cls, edge_builders: list) -> list:
+        for edge_builder in edge_builders:
+            if not isinstance(edge_builder, EdgeBuilderSchemas):
+                LOGGER.warning("%s not defined in anemoi.", edge_builder)
+        return edge_builders
 
 
 class BaseGraphSchema(BaseModel):
-
     nodes: dict[str, NodeSchema]
+    "Nodes schema for all types of nodes (ex. data, hidden)."
     edges: list[EdgeSchema]
-    overwrite: bool = True
-    data: str = "data"
-    hidden: str = "hidden"
+    "List of edges schema."
+    overwrite: bool = Field(default=True)
+    "whether to overwrite existing graph file. Default to True."
+    data: str = Field(default="data")
+    "Key name for the data nodes. Default to 'data'."
+    hidden: str = Field(default="hidden")
+    "Key name for the hidden nodes. Default to 'hidden'."
     # TODO(Helen): Needs to be adjusted for more complex graph setups
--- training/src/anemoi/training/schemas/graphs/edge_schemas.py
+++ training/src/anemoi/training/schemas/graphs/edge_schemas.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2024 ECMWF.
+# (C) Copyright 2024-2025 ECMWF.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -11,53 +11,71 @@
 from __future__ import annotations
 
 from typing import Literal
+from typing import Union
 
 from pydantic import BaseModel
 from pydantic import Field
 from pydantic import PositiveFloat
 from pydantic import PositiveInt
-from pydantic import field_validator
 
 
 class KNNEdgeSchema(BaseModel):
-    target_: str = Field("anemoi.graphs.edges.KNNEdges", alias="_target_")
+    target_: Literal["anemoi.graphs.edges.KNNEdges"] = Field("anemoi.graphs.edges.KNNEdges", alias="_target_")
+    "KNN based edges implementation from anemoi.graphs.edges."
     num_nearest_neighbours: PositiveInt = Field(default=3)
-
-    @field_validator("target_")
-    @classmethod
-    def check_valid_target(cls, target: str) -> str:
-        assert target == "anemoi.graphs.edges.KNNEdges"
-        return target
+    "Number of nearest neighbours. Default to 3."
+    source_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to source nodes of the edges. Default to None."
+    target_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to target nodes of the edges. Default to None."
 
 
 class CutoffEdgeSchema(BaseModel):
-    target_: str = Field("anemoi.graphs.edges.CutOffEdges", alias="_target_")
+    target_: Literal["anemoi.graphs.edges.CutOffEdges"] = Field("anemoi.graphs.edges.CutOffEdges", alias="_target_")
+    "Cut-off based edges implementation from anemoi.graphs.edges."
     cutoff_factor: PositiveFloat = Field(default=0.6)
-
-    @field_validator("target_")
-    @classmethod
-    def check_valid_target(cls, target: str) -> str:
-        assert target == "anemoi.graphs.edges.CutOffEdges"
-        return target
+    "Factor to multiply the grid reference distance to get the cut-off radius. Default to 0.6."
+    source_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to source nodes of the edges. Default to None."
+    target_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to target nodes of the edges. Default to None."
 
 
 class MultiScaleEdgeSchema(BaseModel):
-    target_: str = Field("anemoi.graphs.edges.MultiScaleEdges", alias="_target_")
+    target_: Literal["anemoi.graphs.edges.MultiScaleEdges"] = Field(
+        "anemoi.graphs.edges.MultiScaleEdges",
+        alias="_target_",
+    )
+    "Multi-casle edges implementation from anemoi.graphs.edges."
     x_hops: PositiveInt = Field(default=1)
+    "Number of hops (in the refined icosahedron) between two nodes to connect them with an edge. Default to 1."
+    source_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to source nodes of the edges. Default to None."
+    target_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to target nodes of the edges. Default to None."
 
-    @field_validator("target_")
-    @classmethod
-    def check_valid_target(cls, target: str) -> str:
-        assert target == "anemoi.graphs.edges.MultiScaleEdges"
-        return target
+
+class ICONTopologicalEdgeSchema(BaseModel):
+    target_: Literal[
+        "anemoi.graphs.edges.ICONTopologicalProcessorEdges",
+        "anemoi.graphs.edges.ICONTopologicalEncoderEdges",
+        "anemoi.graphs.edges.ICONTopologicalDecoderEdges",
+    ] = Field("anemoi.graphs.edges.ICONTopologicalProcessorEdges", alias="_target_")
+    icon_mesh: str
+    "The name of the ICON mesh (defines both the processor mesh and the data)."
+    source_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to source nodes of the edges. Default to None."
+    target_mask_attr_name: str | None = Field(default=None, examples=["boundary_mask"])
+    "Mask to apply to target nodes of the edges. Default to None."
 
 
 class EdgeAttributeSchema(BaseModel):
-    target_: str = Field("anemoi.graphs.edges.attributes.EdgeLength", alias="_target_")
+    target_: Literal["anemoi.graphs.edges.attributes.EdgeLength", "anemoi.graphs.edges.attributes.EdgeDirection"] = (
+        Field("anemoi.graphs.edges.attributes.EdgeLength", alias="_target_")
+    )
+    "Edge attributes object from anemoi.graphs.edges."
     norm: Literal["unit-max", "l1", "l2", "unit-sum", "unit-std"] = Field(default="unit-std")
+    "Normalisation method applied to the edge attribute."
 
-    @field_validator("target_")
-    @classmethod
-    def check_valid_target(cls, target: str) -> str:
-        assert target in ["anemoi.graphs.edges.attributes.EdgeLength", "anemoi.graphs.edges.attributes.EdgeDirection"]
-        return target
+
+EdgeBuilderSchemas = Union[KNNEdgeSchema | CutoffEdgeSchema | MultiScaleEdgeSchema | ICONTopologicalEdgeSchema]
