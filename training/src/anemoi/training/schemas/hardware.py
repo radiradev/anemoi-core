5fa2d61541185b0b896cbfe176139bd8bf2c12e2
chebertpinard <charlie.hebert-pinard@ec.gc.ca>
2025-01-10 14:17:45 +0000

    docs: complete description to hardware config schema.

--- training/src/anemoi/training/schemas/hardware.py
+++ training/src/anemoi/training/schemas/hardware.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2024 ECMWF.
+# (C) Copyright 2024-2025 ECMWF.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -27,45 +27,50 @@ from anemoi.training.schemas.utils import allowed_values
 class Checkpoint(BaseModel):
     # TODO(HELEN): Discuss merging with diagnostics checkpoint.
     every_n_epochs: str = "anemoi-by_epoch-epoch_{epoch:03d}-step_{step:06d}"
+    "File name pattern for checkpoint files saved by epoch frequency."
     every_n_train_steps: str = "anemoi-by_step-epoch_{epoch:03d}-step_{step:06d}"
+    "File name pattern for checkpoint files saved by step frequency."
     every_n_minutes: str = "anemoi-by_time-epoch_{epoch:03d}-step_{step:06d}"
+    "File name pattern for checkpoint files saved by time frequency (minutes)."
 
 
 class FilesConfig(BaseModel):
     dataset: Path  # TODO(Helen): Change to FilePath, only posisble after refactor
-    "Path to the dataset file"
+    "Path to the dataset file."
     graph: Path | None = Field(default=None)
-    "Path to the graph file"
+    "Path to the graph file."
     checkpoint: dict[str, str]
-    "Each dictionary key is a checkpoint name, and the value is the path to the checkpoint file"
+    "Each dictionary key is a checkpoint name, and the value is the path to the checkpoint file."
     warm_start: str | None = None
 
 
 class Logs(BaseModel):
     # TODO(Helen): Discuss merging with logging in diagnsotics
-    base: DirectoryPath | None = None
     wandb: DirectoryPath | None = None
+    "Path to output wandb logs."
     mlflow: DirectoryPath | None = None
+    "Path to output mlflow logs."
     tensorboard: DirectoryPath | None = None
+    "Path to output tensorboard logs."
 
 
 class PathsConfig(BaseModel):
     data: DirectoryPath
-    "Path to the data directory"
+    "Path to the data directory."
     grids: DirectoryPath
-    "Path to the grids directory"
+    "Path to the grids directory."
     graph: DirectoryPath
-    "Path to the graph directory"
+    "Path to the graph directory."
     output: Path
-    "Path to the output directory"
+    "Path to the output directory."
     logs: Logs
-    "Logging directories"
+    "Logging directories."
     checkpoints: Path
-    "Path to the checkpoints directory"
+    "Path to the checkpoints directory."
     plots: Path
-    "Path to the plots directory"
+    "Path to the plots directory."
     profiler: Path
-    "Path to the profiler directory"
+    "Path to the profiler directory."
 
 
 class HardwareSchema(BaseModel):
@@ -73,17 +78,17 @@ class HardwareSchema(BaseModel):
         str,
         AfterValidator(partial(allowed_values, values=["cpu", "gpu", "auto", "cuda", "tpu"])),
     ] = "auto"
-    "Accelerator to use for training"
+    "Accelerator to use for training."
     num_gpus_per_node: NonNegativeInt = 1
-    "Number of GPUs per node"
+    "Number of GPUs per node."
     num_nodes: NonNegativeInt = 1
-    "Number of nodes"
+    "Number of nodes."
     num_gpus_per_model: NonNegativeInt = 1
-    "Number of GPUs per model"
+    "Number of GPUs per model."
     files: FilesConfig
-    "Files"
+    "Files schema."
     paths: PathsConfig
-    "Paths"
+    "Paths schema."
 
     @field_validator("num_gpus_per_node")
     @classmethod
