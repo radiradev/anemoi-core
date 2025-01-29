f183e0414509e57c0d86526a4bd5838c828a23a6
Lorenzo Zampieri <lorenzo.zampieri@ecmwf.int>
2025-01-29 16:04:34 +0000

    NormReluBounding definitely fixed for multiple variable case

--- models/src/anemoi/models/layers/bounding.py
+++ models/src/anemoi/models/layers/bounding.py
@@ -136,6 +136,8 @@ class NormalizedReluBounding(BaseBounding):
                 "The length of the min_val list must match the number of variables in NormalizedReluBounding."
             )
 
+        # Redefine the data index to include the variables that will be bounded in the right order
+        self.data_index = [name_to_index[var] for var in variables]
         # Compute normalized min values
         self.norm_min_val = torch.zeros(len(variables))
         for ii, variable in enumerate(variables):
