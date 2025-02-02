dba0edf2f8142dc027f29c14e20cd10ce8c85d1d
Ana Prieto Nemesio <aprieton@lrdn3363.leonardo.local>
2025-02-02 20:17:08 +0100

    check code is running with concat

--- models/src/anemoi/models/layers/embedder.py
+++ models/src/anemoi/models/layers/embedder.py
@@ -5,23 +5,23 @@ import numpy as np
 import torch
 import torch.nn as nn
 from anemoi.models.layers.fourier import levels_expansion
-
+import einops
 
 class VerticalInformationEmbedder(nn.Module):
-
     def __init__(self, config:dict,data_indices) -> None:
         """Initialise.
         """
         super().__init__()
-        self.shuffling = config.level_shuffle 
+        self.level_shuffle = config.level_shuffle 
         self.method = config.method
         self.data_indices = data_indices
-        N_ATMOS_VARIABLES = 6
+        self.vertical_embeddings_method = config.method
 
         #! FOURIER AND ENCODED DIMENSIONS FOR PRESSURE LEVELS
         self.fourier_dim = config.fourier_dim # small otherwise CUDA OOM
-        self.hidden_dim = configs.hidden_dim
+        self.hidden_dim = config.hidden_dim
         self.encoded_dim =config.encoded_dim # small otherwise CUDA OOM
+        self.num_levels = config.num_levels
 
         self.mlp = self._define_mlp()
 
@@ -36,16 +36,17 @@ class VerticalInformationEmbedder(nn.Module):
             level_list.append(numeric_part)
         level_tensor = torch.tensor(level_list)
         if self.level_shuffle:
-            level_tensor= level_tensor.reshape((N_ATMOS_VARIABLES,self.num_levels))[...,self.rand_idx].ravel()
+            level_tensor= level_tensor.reshape((self.num_variables,self.num_levels))[...,self.rand_idx].ravel()
         return level_tensor
 
-    def _shuffle_input(self):
+    def _shuffle_input(self,x_reshaped):
         self.rand_idx = torch.randperm(self.num_levels)
         num_list = torch.Tensor([i for i in range(self.num_levels)])
-        comb_list = torch.cat([rand_idx.view(self.num_levels, 1), num_list.view(self.num_levels, 1)], axis = 1)
+        comb_list = torch.cat([self.rand_idx.view(self.num_levels, 1), num_list.view(self.num_levels, 1)], axis = 1)
         sorted_comb_list = comb_list[comb_list[:, 0].sort()[1]]
         self.rand_rev = [int(i) for i in sorted_comb_list[:, 1]]
-        x_rand = x_reshaped[..., rand_idx]
+        x_rand = x_reshaped[..., self.rand_idx]
+        return x_rand
 
     def _define_mlp(self):
         # Construct MLP to transform vertical encodings
@@ -58,7 +59,7 @@ class VerticalInformationEmbedder(nn.Module):
 
     def _encode_vertical_levels(self,level_tensor):
         #!1st ADDED Fourier transform
-        vertical_encodings = levels_expansion(level_tensor, self.fourier_dim)
+        vertical_encodings = levels_expansion(level_tensor, self.fourier_dim).to("cuda")
 
         #!2nd APPLY MLP
         mapped_vertical_features = self.mlp(vertical_encodings).ravel().to("cuda")
@@ -70,9 +71,9 @@ class VerticalInformationEmbedder(nn.Module):
         n_times = x.shape[1]
         ensemble_size = x.shape[2]
         num_grid_points = x.shape[3]        
-        num_variables = int(x.shape[4]/self.num_levels)
+        self.num_variables = int(x.shape[4]/self.num_levels)
 
-        x_reshaped = torch.reshape(x, (batch_size, n_times, ensemble_size, num_grid_points, num_variables, self.num_levels))
+        x_reshaped = torch.reshape(x, (batch_size, n_times, ensemble_size, num_grid_points, self.num_variables, self.num_levels))
         if self.level_shuffle:
             x_rand = self._shuffle_input(x_reshaped)
         else:
@@ -81,17 +82,25 @@ class VerticalInformationEmbedder(nn.Module):
         mapped_vertical_features= self._encode_vertical_levels(self._get_levels_tensor())
 
         if self.vertical_embeddings_method == 'concat':
-            ## TODO: Can you check this please Ana?
-            mapped_vertical_features = mapped_vertical_features.view(1, 1, 1, 1, num_variables, self.num_levels).expand(
-                batch_size, n_times, 1, num_grid_points, num_variables, self.num_levels, -1
-            ) # ([4, 2, 1, 40320, 6, 13, 99*self.encoded_dim]
-            x_data_vertical_latent = torch.cat((x, mapped_vertical_features), dim=-1)
+            mapped_vertical_features = mapped_vertical_features.view(1, 1, 1, 1, mapped_vertical_features.shape[0])
+            mapped_vertical_features= mapped_vertical_features.reshape(1,1,1,1,self.num_variables,self.num_levels,self.encoded_dim)
+            mapped_vertical_features=mapped_vertical_features.expand(batch_size, n_times, ensemble_size, num_grid_points, self.num_variables, self.num_levels,self.encoded_dim)
+            # MIGHT NEED CONCAT DIM 
+            # CHECK OTHER BRANHC FOR HOW I DID IT THE OTHER DAY
+            x_data_vertical_latent = torch.cat((x_rand.unsqueeze(-1), mapped_vertical_features), dim=-1)
+            return einops.rearrange(
+                    x_data_vertical_latent, "batch time ensemble grid vars levels concatdim -> (batch ensemble grid) (time vars levels concatdim)"
+                )
         elif self.vertical_embeddings_method == 'addition':
-            mapped_vertical_features = mapped_vertical_features.view(1, 1, 1, 1, num_variables, self.num_levels).expand(
-                batch_size, n_times, 1, num_grid_points, num_variables, self.num_levels
+            mapped_vertical_features = mapped_vertical_features.view(1, 1, 1, 1, self.num_variables, self.num_levels).expand(
+                batch_size, n_times, 1, num_grid_points, self.num_variables, self.num_levels
             ) # ([4, 2, 1, 40320, 6, 13]
             x_data_vertical_latent = x_rand + mapped_vertical_features
+
+            # pressure levels
+            return einops.rearrange(
+                    x_data_vertical_latent, "batch time ensemble grid vars levels -> (batch ensemble grid) (time vars levels)"
+                )
         else:
             raise ValueError(f"Unknown vertical embeddings method: {self.vertical_embeddings_method}")
 
-        return x_data_vertical_latent
