d6d7f152ec692d3ec362c4737f745d83dfed1dc3
Icedoom <9220778+icedoom888@users.noreply.github.com>
2025-02-03 15:26:09 +0100

    feat(graphs): New Edge Attribute: AttributeFromNode (#62)
    * Implemented new attribute
    
    * changelog update
    
    * changelog update
    
    * Refactored following review
    
    * Update src/anemoi/graphs/edges/attributes.py
    
    Co-authored-by: Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
    
    * Update src/anemoi/graphs/edges/attributes.py
    
    Co-authored-by: Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
    
    * Update src/anemoi/graphs/edges/attributes.py
    
    Co-authored-by: Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
    
    * Update src/anemoi/graphs/edges/attributes.py
    
    Co-authored-by: Mario Santa Cruz <48736305+JPXKQX@users.noreply.github.com>
    
    * Refactor after review
    
    * Docstring done
    
    * Docstring done
    
    * Docstring done
    
    * Fixed ABC issue
    
    * addressed changes in docs and exception error
    
    * Changed changelog
    
    * TMP
    
    * Addressed comments
    
    * gpc
    
    * Removed update to changelog
    
    * Added test for fail copy
    
    * Gpc passed
    
    * Fixed test
    
    * Fixed test
    
    * Test working now

--- graphs/tests/edges/test_edge_attributes.py
+++ graphs/tests/edges/test_edge_attributes.py
@@ -7,9 +7,13 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+from functools import partial
+
 import pytest
 import torch
 
+from anemoi.graphs.edges.attributes import AttributeFromSourceNode
+from anemoi.graphs.edges.attributes import AttributeFromTargetNode
 from anemoi.graphs.edges.attributes import EdgeDirection
 from anemoi.graphs.edges.attributes import EdgeLength
 
@@ -36,3 +40,17 @@ def test_fail_edge_features(attribute_builder, graph_nodes_and_edges):
     """Test edge attribute builder fails with unknown nodes."""
     with pytest.raises(AssertionError):
         attribute_builder.compute(graph_nodes_and_edges, ("test_nodes", "to", "unknown_nodes"))
+
+
+@pytest.mark.parametrize(
+    "attribute_builder",
+    [
+        partial(AttributeFromSourceNode, node_attr_name="example_attr"),
+        partial(AttributeFromTargetNode, node_attr_name="example_attr"),
+    ],
+)
+def test_fail_edge_attribute_from_node(attribute_builder, graph_nodes_and_edges):
+    """Test edge attribute builder fails with unknown nodes."""
+    with pytest.raises(KeyError):
+        builder_instance = attribute_builder()
+        builder_instance.compute(graph_nodes_and_edges, ("test_nodes", "to", "test_nodes"))
