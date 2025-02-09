5927a2d74e763247228ecbbd6fad4b5dbc8c4343
obrien2 <obrien2@jpbot-001-01.jupiter.internal>
2025-02-09 15:36:36 +0100

    changed to subclassing tensor

--- training/src/anemoi/training/utils/pinned_tensor.py
+++ training/src/anemoi/training/utils/pinned_tensor.py
@@ -16,9 +16,6 @@ import torch
 import torch.nn as nn
 import torch._C as _C
 
-from anemoi.training.train.forecaster import GraphForecaster
-from anemoi.utils.checkpoints import save_metadata
-
 LOGGER = logging.getLogger(__name__)
 
 #class SmartTensor(torch.Tensor):
@@ -57,7 +54,8 @@ LOGGER = logging.getLogger(__name__)
         #return result
 
 #https://pytorch.org/docs/stable/notes/extending.html#extending-torch-with-a-tensor-wrapper-type
-class PinnedTensor(object):
+#class PinnedTensor(object):
+class PinnedTensor(torch.Tensor):
     '''
     Extension of the default torch.Tensor
     The tensor will be resident on pinned memory on the CPU.
@@ -65,13 +63,27 @@ class PinnedTensor(object):
     before being deleted. The result tensor will be stored on device
     This Tensor class is ideal for large, infrequently accessed data (e.g. copies of the input batch at 9km)
     '''
-    def __init__(self, data, pinned=False, **kwargs):
+
+    @staticmethod
+    def __new__(cls, data, pinned=False, *args, **kwargs):
+        if pinned:
+            data = data.to("cpu").pin_memory()  # Allocate in pinned memory
+        return super().__new__(cls, data, *args, **kwargs)
+
+    def __init__(self, data, pinned=True, **kwargs):
         self._pinned = pinned
-        if self._pinned:
-            #self._t = torch.tensor(data.pin_memory(), device="cpu", pin_memory=True, **kwargs)
-            self._t = torch.as_tensor(data.pin_memory(), **kwargs)
-        else:
-            self._t = torch.tensor(data, **kwargs)
+        #if self._pinned:
+        #    #self._t = torch.tensor(data.pin_memory(), device="cpu", pin_memory=True, **kwargs)
+        #    self._t = torch.as_tensor(data.to("cpu").pin_memory(), **kwargs)
+        #else:
+        #    self._t = torch.tensor(data, **kwargs)
+
+    #def __getattr__(self, name):
+    #    """Delegate attribute access to the underlying tensor."""
+    #    return getattr(self._t, name)
+
+    #def __repr__(self):
+    #    return f"PinnedTensor({self._t})"
 
     @classmethod
     def __torch_function__(cls, func, types, args=(), kwargs=None):
@@ -80,21 +92,36 @@ class PinnedTensor(object):
 
         pinneds = tuple(a._pinned for a in args if hasattr(a, '_pinned'))
         #metadatas = tuple(a._metadata for a in args if hasattr(a, '_metadata'))
-        args = [getattr(a, '_t', a) for a in args]
+        #args = [getattr(a, '_t', a) for a in args]
+
+        # you need to guard against every tensor function you invoke in __torch_function__, before you invoke it
+        # That includes __repr__ if you want to print
+        # If we dont guard against to(), then we recurse infinitely below
+        if func == torch.Tensor.to:
+            return super().__torch_function__(func, types, args, kwargs)
 
         processed=[]
         for arg in args:
-            processed.append(arg.to("cuda", non_blocking=True)) #assuming this does nothing if 'arg' is already on 'cuda'
-        return func(*processed, **kwargs)
+            if type(arg) == PinnedTensor:
+                processed.append(arg.to("cuda", non_blocking=True)) 
+            else:
+                processed.append(arg)
+
+        return super().__torch_function__(func, types, processed, kwargs)
 
 def demo():
     print("An example of 'PinnedTensor(torch.randn(5, 5)) + torch.randn(5, 5, device='cuda')'")
+    #data = torch.randn(5, 5, device="cuda")  # Random tensor
     data = torch.randn(5, 5)  # Random tensor
     pinned_tensor = PinnedTensor(data, pinned=True)
+    print(pinned_tensor.shape) #demonstrates that tensor attributes are passed off to the underlying tensor
 
     gpu_data=torch.randn(5, 5, device="cuda")
     result = pinned_tensor + gpu_data
 
-    print(f"before: {pinned_tensor._t=}, {gpu_data=}. \nafter: {result=}")
+    print(f"before: {pinned_tensor=}, {gpu_data=}. \nafter: {result=}") #demonstrates a cpu tensor being automatically moved to cuda and the result kept there
 
-demo()
+    import einops
+    einops.rearrange(pinned_tensor, "batch time -> (batch time)")
+
+#demo()
