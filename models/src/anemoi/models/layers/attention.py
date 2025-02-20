34cf4ff71c5eebacd56666bfd7c978f51cab332e
Rilwan Adewoyin <18564167+Rilwan-Adewoyin@users.noreply.github.com>
2025-02-20 16:44:23 +0000

    Added Cross Attention Module

--- models/src/anemoi/models/layers/attention.py
+++ models/src/anemoi/models/layers/attention.py
@@ -110,3 +110,90 @@ class MultiHeadSelfAttention(nn.Module):
         out = self.projection(out)
 
         return out
+
+class MultiHeadCrossAttention(nn.Module):
+    """Multi Head Cross Attention Pytorch Layer."""
+
+    def __init__(
+        self,
+        num_heads: int,
+        embed_dim: int,
+        bias: bool = False,
+        is_causal: bool = False,
+        window_size: Optional[int] = None,
+        dropout_p: float = 0.0,
+    ):
+        super().__init__()
+
+        assert (
+            embed_dim % num_heads == 0
+        ), f"Embedding dimension ({embed_dim}) must be divisible by number of heads ({num_heads})"
+
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        self.head_dim = embed_dim // num_heads  # q k v
+        self.window_size = (window_size, window_size)  # flash attention
+        self.dropout_p = dropout_p
+        self.is_causal = is_causal
+
+        self.lin_q = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.lin_k = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.lin_v = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.attention = attn_func
+
+        if not _FLASH_ATTENTION_AVAILABLE:
+            LOGGER.warning("Flash attention not available, falling back to pytorch scaled_dot_product_attention")
+
+        self.projection = nn.Linear(embed_dim, embed_dim, bias=True)
+
+
+    def forward(
+        self, x: list[Tensor], shapes: list, batch_size: int, model_comm_group: Optional[ProcessGroup] = None
+    ) -> Tensor:
+        # query, key, value = self.lin_qkv(x).chunk(3, -1)
+
+        query = self.lin_q(x[1])
+        key = self.lin_k(x[0])
+        value = self.lin_v(x[0])
+
+        if model_comm_group:
+            assert (
+                model_comm_group.size() == 1 or batch_size == 1
+            ), "Only batch size of 1 is supported when model is sharded accross GPUs"
+
+        query, key, value = (
+            einops.rearrange(
+                t,
+                "(batch grid) (heads vars) -> batch heads grid vars",
+                batch=batch_size,
+                heads=self.num_heads,
+            )
+            for t in (query, key, value)
+        )
+
+        query = shard_heads(query, shapes=shapes, mgroup=model_comm_group)
+        key = shard_heads(key, shapes=shapes, mgroup=model_comm_group)
+        value = shard_heads(value, shapes=shapes, mgroup=model_comm_group)
+        dropout_p = self.dropout_p if self.training else 0.0
+
+        if _FLASH_ATTENTION_AVAILABLE:
+            query, key, value = (
+                einops.rearrange(t, "batch heads grid vars -> batch grid heads vars") for t in (query, key, value)
+            )
+            out = self.attention(query, key, value, causal=False, window_size=self.window_size, dropout_p=dropout_p)
+            out = einops.rearrange(out, "batch grid heads vars -> batch heads grid vars")
+        else:
+            out = self.attention(
+                query,
+                key,
+                value,
+                is_causal=False,
+                dropout_p=dropout_p,
+            )  # expects (batch heads grid variable) format
+
+        out = shard_sequence(out, shapes=shapes, mgroup=model_comm_group)
+        out = einops.rearrange(out, "batch heads grid vars -> (batch grid) (heads vars)")
+
+        out = self.projection(out)
+
+        return out
