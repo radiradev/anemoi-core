7b2af248c200c210d9dabf99ac742b79f5fbd64f
Simon Lang <simon.lang@ecmwf.int>
2025-03-10 15:05:55 +0000

    compile transformer gnn

--- models/src/anemoi/models/layers/conv.py
+++ models/src/anemoi/models/layers/conv.py
@@ -22,6 +22,7 @@ from torch_geometric.utils import scatter
 from torch_geometric.utils import softmax
 
 from anemoi.models.layers.mlp import MLP
+from anemoi.models.compile.decorators import torch_compile
 from anemoi.utils.config import DotDict
 
 
@@ -81,7 +82,7 @@ class GraphConv(MessagePassing):
 
         return out, edges_new
 
-
+@torch_compile()
 class GraphTransformerConv(MessagePassing):
     """Message passing part of graph transformer operator.
 
