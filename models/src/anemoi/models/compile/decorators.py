7c51510560b0d04dccbf0317243c03a439199dde
Harrison Cook <harrison.cook@ecmwf.int>
2025-03-20 11:58:29 +0000

    Add logging to compile

--- models/src/anemoi/models/compile/decorators.py
+++ models/src/anemoi/models/compile/decorators.py
@@ -7,6 +7,10 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+import logging
+
+LOG = logging.getLogger(__name__)
+
 
 def torch_compile(dynamic=True, **kwargs):
     """
@@ -26,6 +30,8 @@ def torch_compile(dynamic=True, **kwargs):
         if req:
             compile_func = torch.compile(dynamic=dynamic, **kwargs)
             return compile_func(func)
+
+        LOG.debug("Requirements not met for torch.compile, returning original function.")
         return func
 
     return decorator
