radiradev/anemoi-core

commit 4684b1c7a60495bc3a32946b9bf780aef31e387a

feat(training): autoencoder 🗜️ (#172)

models/src/anemoi/models/models/autoencoder.py
@@ -1,0 +1,263 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import logging
+from typing import Optional
+
+import einops
+import torch
+from torch import Tensor
+from torch.distributed.distributed_c10d import ProcessGroup
+from torch_geometric.data import HeteroData
+
+from anemoi.models.distributed.shapes import get_shape_shards
+from anemoi.models.models import AnemoiModelEncProcDec
+from anemoi.models.models import AnemoiModelEncProcDecHierarchical
+from anemoi.utils.config import DotDict
+
+LOGGER = logging.getLogger(__name__)
+
+
+class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
+    """Message passing graph neural network."""
+
+    def __init__(
+        self,
+        *,
+        model_config: DotDict,
+        data_indices: dict,
+        statistics: dict,
+        graph_data: HeteroData,
+    ) -> None:
+        """Initializes the graph neural network.
+
+        Parameters
+        ----------
+        model_config : DotDict
+            Model configuration
+        data_indices : dict
+            Data indices
+        graph_data : HeteroData
+            Graph definition
+        """
+        super().__init__(
+            model_config=model_config, data_indices=data_indices, statistics=statistics, graph_data=graph_data
+        )
+
+    def forward(self, x: Tensor, model_comm_group: Optional[ProcessGroup] = None) -> Tensor:
+        batch_size = x.shape[0]
+        ensemble_size = x.shape[2]
+
+        # add data positional info (lat/lon)
+        x_data_latent = torch.cat(
+            (
+                einops.rearrange(x, "batch time ensemble grid vars -> (batch ensemble grid) (time vars)"),
+                self.node_attributes(self._graph_name_data, batch_size=batch_size),
+            ),
+            dim=-1,  # feature dimension
+        )
+
+        x_hidden_latent = self.node_attributes(self._graph_name_hidden, batch_size=batch_size)
+
+        # get shard shapes
+        print("\nData shape: ", x_data_latent.shape)
+        shard_shapes_data = get_shape_shards(x_data_latent, 0, model_comm_group)
+        print("\nHidden shape: ", x_hidden_latent.shape)
+        shard_shapes_hidden = get_shape_shards(x_hidden_latent, 0, model_comm_group)
+
+        print("\nEncoder shapes: ", (shard_shapes_data, shard_shapes_hidden))
+        # Run encoder
+        x_data_latent, x_latent = self._run_mapper(
+            self.encoder,
+            (x_data_latent, x_hidden_latent),
+            batch_size=batch_size,
+            shard_shapes=(shard_shapes_data, shard_shapes_hidden),
+            model_comm_group=model_comm_group,
+        )
+
+        # Run decoder
+        x_out = self._run_mapper(
+            self.decoder,
+            (x_latent, x_data_latent),
+            batch_size=batch_size,
+            shard_shapes=(shard_shapes_hidden, shard_shapes_data),
+            model_comm_group=model_comm_group,
+        )
+
+        x_out = (
+            einops.rearrange(
+                x_out,
+                "(batch ensemble grid) vars -> batch ensemble grid vars",
+                batch=batch_size,
+                ensemble=ensemble_size,
+            )
+            .to(dtype=x.dtype)
+            .clone()
+        )
+
+        for bounding in self.boundings:
+            # bounding performed in the order specified in the config file
+            x_out = bounding(x_out)
+
+        return x_out
+
+
+class AnemoiModelHierarchicalAutoEncoder(AnemoiModelEncProcDecHierarchical):
+
+    def __init__(
+        self,
+        *,
+        model_config: DotDict,
+        data_indices: dict,
+        statistics: dict,
+        graph_data: HeteroData,
+    ) -> None:
+        """Initializes the graph neural network.
+
+        Parameters
+        ----------
+        model_config : DotDict
+            Model configuration
+        data_indices : dict
+            Data indices
+        graph_data : HeteroData
+            Graph definition
+        """
+        super().__init__(
+            model_config=model_config, data_indices=data_indices, statistics=statistics, graph_data=graph_data
+        )
+
+    def forward(self, x: Tensor, model_comm_group: Optional[ProcessGroup] = None) -> Tensor:
+        batch_size = x.shape[0]
+        ensemble_size = x.shape[2]
+
+        # add data positional info (lat/lon)
+        x_trainable_data = torch.cat(
+            (
+                einops.rearrange(x, "batch time ensemble grid vars -> (batch ensemble grid) (time vars)"),
+                self.node_attributes(self._graph_name_data, batch_size=batch_size),
+            ),
+            dim=-1,  # feature dimension
+        )
+
+        # Get all trainable parameters for the hidden layers -> initialisation of each hidden, which becomes trainable bias
+        x_trainable_hiddens = {}
+        for hidden in self._graph_hidden_names:
+            x_trainable_hiddens[hidden] = self.node_attributes(hidden, batch_size=batch_size)
+
+        # Get data and hidden shapes for sharding
+        print("\nData shape: ", x_trainable_data.shape)
+        shard_shapes_data = get_shape_shards(x_trainable_data, 0, model_comm_group)
+        shard_shapes_hiddens = {}
+        for hidden, x_latent in x_trainable_hiddens.items():
+            print(f"{hidden} shape: ", x_latent.shape)
+            shard_shapes_hiddens[hidden] = get_shape_shards(x_latent, 0, model_comm_group)
+
+        print("\nEncoder shapes: ", (shard_shapes_data, shard_shapes_hiddens[self._graph_hidden_names[0]]))
+        # Run encoder
+        x_data_latent, curr_latent = self._run_mapper(
+            self.encoder,
+            (x_trainable_data, x_trainable_hiddens[self._graph_hidden_names[0]]),
+            batch_size=batch_size,
+            shard_shapes=(shard_shapes_data, shard_shapes_hiddens[self._graph_hidden_names[0]]),
+            model_comm_group=model_comm_group,
+        )
+
+        # Run processor
+        x_encoded_latents = {}
+        x_skip = {}
+
+        ## Downscale
+        for i in range(0, self.num_hidden - 1):
+            src_hidden_name = self._graph_hidden_names[i]
+            dst_hidden_name = self._graph_hidden_names[i + 1]
+
+            # Processing at same level
+            if self.level_process:
+                curr_latent = self.down_level_processor[src_hidden_name](
+                    curr_latent,
+                    batch_size=batch_size,
+                    shard_shapes=shard_shapes_hiddens[src_hidden_name],
+                    model_comm_group=model_comm_group,
+                )
+
+            # store latents for skip connections
+            x_skip[src_hidden_name] = curr_latent
+
+            # Encode to next hidden level
+            print(
+                f"Downscale {i} shapes: ",
+                (shard_shapes_hiddens[src_hidden_name], shard_shapes_hiddens[dst_hidden_name]),
+            )
+
+            x_encoded_latents[src_hidden_name], curr_latent = self._run_mapper(
+                self.downscale[src_hidden_name],
+                (curr_latent, x_trainable_hiddens[dst_hidden_name]),
+                batch_size=batch_size,
+                shard_shapes=(shard_shapes_hiddens[src_hidden_name], shard_shapes_hiddens[dst_hidden_name]),
+                model_comm_group=model_comm_group,
+            )
+
+        ## Upscale
+        for i in range(self.num_hidden - 1, 0, -1):
+            src_hidden_name = self._graph_hidden_names[i]
+            dst_hidden_name = self._graph_hidden_names[i - 1]
+
+            # Decode to next level
+            print(
+                f"Upscale {i} shapes: ", (shard_shapes_hiddens[src_hidden_name], shard_shapes_hiddens[dst_hidden_name])
+            )
+
+            curr_latent = self._run_mapper(
+                self.upscale[src_hidden_name],
+                (curr_latent, x_encoded_latents[dst_hidden_name]),
+                batch_size=batch_size,
+                shard_shapes=(shard_shapes_hiddens[src_hidden_name], shard_shapes_hiddens[dst_hidden_name]),
+                model_comm_group=model_comm_group,
+            )
+
+            # Processing at same level
+            if self.level_process:
+                curr_latent = self.up_level_processor[dst_hidden_name](
+                    curr_latent,
+                    batch_size=batch_size,
+                    shard_shapes=shard_shapes_hiddens[dst_hidden_name],
+                    model_comm_group=model_comm_group,
+                )
+
+        # Run decoder
+        print("Decoder shapes: ", (shard_shapes_hiddens[self._graph_hidden_names[0]], shard_shapes_data))
+        x_out = self._run_mapper(
+            self.decoder,
+            (curr_latent, x_data_latent),
+            batch_size=batch_size,
+            shard_shapes=(shard_shapes_hiddens[self._graph_hidden_names[0]], shard_shapes_data),
+            model_comm_group=model_comm_group,
+        )
+
+        x_out = (
+            einops.rearrange(
+                x_out,
+                "(batch ensemble grid) vars -> batch ensemble grid vars",
+                batch=batch_size,
+                ensemble=ensemble_size,
+            )
+            .to(dtype=x.dtype)
+            .clone()
+        )
+
+        # residual connection (just for the prognostic variables)
+        x_out[..., self._internal_output_idx] += x[:, -1, :, :, self._internal_input_idx]
+
+        for bounding in self.boundings:
+            # bounding performed in the order specified in the config file
+            x_out = bounding(x_out)
+
+        return x_out

models/src/anemoi/models/models/encoder_processor_decoder.py
@@ -14,6 +14,7 @@ from typing import Optional
 import einops
 import numpy as np
 import torch
+from hydra.errors import InstantiationException
 from hydra.utils import instantiate
 from torch import Tensor
 from torch import nn
@@ -93,39 +94,55 @@ class AnemoiModelEncProcDec(nn.Module):
             LOGGER.info("Truncation: A_up %s", self.A_up.shape)
 
         # Encoder data -> hidden
-        self.encoder = instantiate(
-            model_config.model.encoder,
-            in_channels_src=self.input_dim,
-            in_channels_dst=self.node_attributes.attr_ndims[self._graph_name_hidden],
-            hidden_dim=self.num_channels,
-            sub_graph=self._graph_data[(self._graph_name_data, "to", self._graph_name_hidden)],
-            src_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
-            dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            layer_kernels=self.layer_kernels_encoder,
-        )
+        try:
+            self.encoder = instantiate(
+                model_config.model.encoder,
+                in_channels_src=self.input_dim,
+                in_channels_dst=self.node_attributes.attr_ndims[self._graph_name_hidden],
+                hidden_dim=self.num_channels,
+                sub_graph=self._graph_data[(self._graph_name_data, "to", self._graph_name_hidden)],
+                src_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
+                dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
+                layer_kernels=self.layer_kernels,
+            )
+
+        except InstantiationException:
+            LOGGER.info(
+                f"Could not instantiate {model_config.model.encoder}, might cause errors later if this module is used."
+            )
 
         # Processor hidden -> hidden
-        self.processor = instantiate(
-            model_config.model.processor,
-            num_channels=self.num_channels,
-            sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_hidden)],
-            src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            layer_kernels=self.layer_kernels_processor,
-        )
+        try:
+            self.processor = instantiate(
+                model_config.model.processor,
+                num_channels=self.num_channels,
+                sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_hidden)],
+                src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
+                dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
+                layer_kernels=self.layer_kernels,
+            )
+        except InstantiationException:
+            LOGGER.info(
+                f"Could not instantiate {model_config.model.processor}, might cause errors later if this module is used."
+            )
 
         # Decoder hidden -> data
-        self.decoder = instantiate(
-            model_config.model.decoder,
-            in_channels_src=self.num_channels,
-            in_channels_dst=self.input_dim,
-            hidden_dim=self.num_channels,
-            out_channels_dst=self.num_output_channels,
-            sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_data)],
-            src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            dst_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
-            layer_kernels=self.layer_kernels_decoder,
-        )
+        try:
+            self.decoder = instantiate(
+                model_config.model.decoder,
+                in_channels_src=self.num_channels,
+                in_channels_dst=self.input_dim,
+                hidden_dim=self.num_channels,
+                out_channels_dst=self.num_output_channels,
+                sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_data)],
+                src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
+                dst_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
+                layer_kernels=self.layer_kernels,
+            )
+        except InstantiationException:
+            LOGGER.info(
+                f"Could not instantiate {model_config.model.decoder}, might cause errors later if this module is used."
+            )
 
         # Instantiation of model output bounding functions (e.g., to ensure outputs like TP are positive definite)
         self.boundings = nn.ModuleList(

training/src/anemoi/training/train/forecaster/autoencoder.py
@@ -1,0 +1,110 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import logging
+from collections.abc import Mapping
+
+import torch
+from torch.utils.checkpoint import checkpoint
+from torch_geometric.data import HeteroData
+
+from anemoi.models.data_indices.collection import IndexCollection
+from anemoi.training.schemas.base_schema import BaseSchema
+
+from .forecaster import GraphForecaster
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GraphAutoEncoder(GraphForecaster):
+    """Graph neural network forecaster for PyTorch Lightning."""
+
+    def __init__(
+        self,
+        *,
+        config: BaseSchema,
+        graph_data: HeteroData,
+        statistics: dict,
+        data_indices: IndexCollection,
+        metadata: dict,
+        supporting_arrays: dict,
+    ) -> None:
+        """Initialize graph neural network forecaster.
+
+        Parameters
+        ----------
+        config : BaseSchema
+            Job configuration
+        graph_data : HeteroData
+            Graph object
+        statistics : dict
+            Statistics of the training data
+        data_indices : IndexCollection
+            Indices of the training data,
+        metadata : dict
+            Provenance information
+        supporting_arrays : dict
+            Supporting NumPy arrays to store in the checkpoint
+
+        """
+        super().__init__(
+            config=config,
+            graph_data=graph_data,
+            statistics=statistics,
+            data_indices=data_indices,
+            metadata=metadata,
+            supporting_arrays=supporting_arrays,
+        )
+
+    def _step(
+        self,
+        batch: torch.Tensor,
+        batch_idx: int,
+        validation_mode: bool = False,
+    ) -> tuple[torch.Tensor, Mapping[str, torch.Tensor]]:
+
+        del batch_idx
+        batch = self.allgather_batch(batch)
+
+        loss = torch.zeros(1, dtype=batch.dtype, device=self.device, requires_grad=False)
+        metrics = {}
+        y_preds = []
+
+        # for validation not normalized in-place because remappers cannot be applied in-place
+        batch = self.model.pre_processors(batch, in_place=not validation_mode)
+
+        if not self.updated_loss_mask:
+            # update loss scalar after first application and initialization of preprocessors
+            self.training_weights_for_imputed_variables(batch)
+
+        # start rollout of preprocessed batch
+        x = batch[
+            :,
+            0 : self.multi_step,
+            ...,
+            self.data_indices.internal_data.input.full,
+        ]  # (bs, multi_step, latlon, nvar)
+
+        y_pred = self(x)
+        y = batch[:, 0, ..., self.data_indices.internal_data.output.full]
+
+        loss += checkpoint(self.loss, y_pred, y, use_reentrant=False)
+
+        metrics_next = {}
+        if validation_mode:
+            metrics_next = self.calculate_val_metrics(
+                y_pred,
+                y,
+                1,
+            )
+
+        metrics.update(metrics_next)
+        y_preds.extend(y_pred)
+
+        return loss, metrics, y_preds

models/src/anemoi/models/models/__init__.py
@@ -7,6 +7,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+from .autoencoder import AnemoiModelAutoEncoder
+from .autoencoder import AnemoiModelHierarchicalAutoEncoder
 from .encoder_processor_decoder import AnemoiModelEncProcDec
 from .ens_encoder_processor_decoder import AnemoiEnsModelEncProcDec
 from .hierarchical import AnemoiModelEncProcDecHierarchical
@@ -14,7 +16,9 @@ from .interpolator import AnemoiModelEncProcDecInterpolator
 
 __all__ = [
     "AnemoiModelEncProcDec",
-    "AnemoiEnsModelEncProcDec",
     "AnemoiModelEncProcDecHierarchical",
     "AnemoiModelEncProcDecInterpolator",
+    "AnemoiEnsModelEncProcDec",
+    "AnemoiModelAutoEncoder",
+    "AnemoiModelHierarchicalAutoEncoder",
 ]

commit ccb2b5ccf53cb4de8db34c56d39ee8fd6e297606

bug-fix - bugs introduced in #172 relating ot layer_kernels init arguments in model instantiation

models/src/anemoi/models/models/autoencoder.py
@@ -33,6 +33,30 @@ LOGGER = logging.getLogger(__name__)
 class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
     """Message passing graph neural network."""
 
+    # def __init__(
+    #     self,
+    #     *,
+    #     model_config: DotDict,
+    #     data_indices: dict,
+    #     statistics: dict,
+    #     graph_data: HeteroData,
+    #     **kwargs,
+    # ) -> None:
+    #     """Initializes the graph neural network.
+
+    #     Parameters
+    #     ----------
+    #     model_config : DotDict
+    #         Model configuration
+    #     data_indices : dict
+    #         Data indices
+    #     graph_data : HeteroData
+    #         Graph definition
+    #     """
+    #     super().__init__(
+    #         model_config=model_config, data_indices=data_indices, statistics=statistics, graph_data=graph_data, **kwargs
+    #     )
+
     def __init__(
         self,
         *,
@@ -53,31 +77,7 @@ class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
         graph_data : HeteroData
             Graph definition
         """
-        super().__init__(
-            model_config=model_config, data_indices=data_indices, statistics=statistics, graph_data=graph_data, **kwargs
-        )
-
-    def __init__(
-        self,
-        *,
-        model_config: DotDict,
-        data_indices: dict,
-        statistics: dict,
-        graph_data: HeteroData,
-        truncation_data: dict,
-    ) -> None:
-        """Initializes the graph neural network.
-
-        Parameters
-        ----------
-        model_config : DotDict
-            Model configuration
-        data_indices : dict
-            Data indices
-        graph_data : HeteroData
-            Graph definition
-        """
-        super().__init__()
+        super(AnemoiModelEncProcDec, self).__init__() #Temporary -> End state is to create Base Class for Models or Mixins
         model_config = DotDict(model_config)
         self._graph_data = graph_data
         self._graph_name_data = model_config.graph.data
@@ -96,14 +96,11 @@ class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
         # read config.model.layer_kernels to get the implementation for certain layers
         self.layer_kernels_encoder = load_layer_kernels(model_config.model.layer_kernels.get("encoder", {}))
         self.layer_kernels_decoder = load_layer_kernels(model_config.model.layer_kernels.get("decoder", {}))
-        self.layer_kernels_processor = load_layer_kernels(model_config.model.layer_kernels.get("processor", {}))
 
         self.multi_step = model_config.training.multistep_input
         self.num_channels = model_config.model.num_channels
 
         self.node_attributes = NamedNodesAttributes(model_config.model.trainable_parameters.hidden, self._graph_data)
-
-        self._truncation_data = truncation_data
 
         self.input_dim = self._calculate_input_dim(model_config)
 
@@ -119,7 +116,7 @@ class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
             sub_graph=self._graph_data[(self._graph_name_data, "to", self._graph_name_hidden)],
             src_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
             dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            layer_kernels=self.layer_kernels,
+            layer_kernels=self.layer_kernels_encoder,
         )
 
         # Decoder hidden -> data
@@ -132,7 +129,7 @@ class AnemoiModelAutoEncoder(AnemoiModelEncProcDec):
             sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_data)],
             src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
             dst_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
-            layer_kernels=self.layer_kernels,
+            layer_kernels=self.layer_kernels_decoder,
         )
 
         # Instantiation of model output bounding functions (e.g., to ensure outputs like TP are positive definite)

models/src/anemoi/models/models/encoder_processor_decoder.py
@@ -103,7 +103,7 @@ class AnemoiModelEncProcDec(nn.Module):
             sub_graph=self._graph_data[(self._graph_name_data, "to", self._graph_name_hidden)],
             src_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
             dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            layer_kernels=self.layer_kernels,
+            layer_kernels=self.layer_kernels_encoder,
         )
 
         # Processor hidden -> hidden
@@ -113,7 +113,7 @@ class AnemoiModelEncProcDec(nn.Module):
             sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_hidden)],
             src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
             dst_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
-            layer_kernels=self.layer_kernels,
+            layer_kernels=self.layer_kernels_processor,
         )
 
         # Decoder hidden -> data
@@ -126,7 +126,7 @@ class AnemoiModelEncProcDec(nn.Module):
             sub_graph=self._graph_data[(self._graph_name_hidden, "to", self._graph_name_data)],
             src_grid_size=self.node_attributes.num_nodes[self._graph_name_hidden],
             dst_grid_size=self.node_attributes.num_nodes[self._graph_name_data],
-            layer_kernels=self.layer_kernels,
+            layer_kernels=self.layer_kernels_decoder,
         )
 
         # Instantiation of model output bounding functions (e.g., to ensure outputs like TP are positive definite)