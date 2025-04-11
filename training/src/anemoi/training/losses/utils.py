Fix print of variable scaling

training/src/anemoi/training/losses/utils.py
@@ -10,16 +10,26 @@
 
 import logging
 
-from torch import nn
-
 from anemoi.models.data_indices.collection import IndexCollection
 from anemoi.training.utils.enums import TensorDim
+
+from anemoi.training.losses.base import BaseLoss
 
 LOGGER = logging.getLogger(__name__)
 
 
-def print_variable_scaling(loss: type[nn.Module], data_indices: IndexCollection) -> None:
-    variable_scaling = loss.scaler.subset_by_dim(TensorDim.VARIABLE.value)
+def print_variable_scaling(loss: BaseLoss, data_indices: IndexCollection) -> None:
+    """
+    Log the final variable scaling for each variable in the model.
+
+    Parameters
+    ----------
+    loss : BaseLoss
+        Loss function to get the variable scaling from.
+    data_indices : IndexCollection
+        Index collection to get the variable names from.
+    """
+    variable_scaling = loss.scaler.resolve(len(TensorDim)).subset_by_dim(TensorDim.VARIABLE.value)
     log_text = "Final Variable Scaling: "
     for idx, name in enumerate(data_indices.internal_model.output.name_to_index.keys()):
         log_text += f"{name}: {variable_scaling[idx]:.4g}, "

training/src/anemoi/training/train/forecaster/forecaster.py
@@ -23,6 +23,7 @@ from anemoi.training.losses import get_loss_function
 from anemoi.training.losses.base import BaseLoss
 from anemoi.training.losses.loss import get_metric_ranges
 from anemoi.training.losses.scaler_tensor import grad_scaler
+from anemoi.training.losses.utils import print_variable_scaling
 from anemoi.training.losses.scalers import create_scalers
 from anemoi.training.schemas.base_schema import BaseSchema
 from anemoi.training.schemas.base_schema import convert_to_omegaconf
@@ -118,7 +119,7 @@ class GraphForecaster(pl.LightningModule):
         )
 
         self.loss = get_loss_function(config.model_dump(by_alias=True).training.training_loss, scalers=self.scalers)
-        # print_variable_scaling(self.loss, data_indices) TODO(Harrison): related to ScalerTensor
+        print_variable_scaling(self.loss, data_indices)
 
         self.metrics = torch.nn.ModuleDict(
             {