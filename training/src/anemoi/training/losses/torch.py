a0a984fc59efe6789d829a312b9ccf76f990b61c
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-07-03 14:28:57 +0000

    support torch loss

---
+++ training/src/anemoi/training/losses/torch.py
@@ -0,0 +1,45 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+from __future__ import annotations
+
+import logging
+from typing import Callable
+import torch
+
+from anemoi.training.losses.base import FunctionalLoss
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TorchLoss(FunctionalLoss):
+    """Loss function"""
+
+    def __init__(self, loss: Callable, ignore_nans: bool = False, **kwargs) -> None:
+        super().__init__(ignore_nans)
+        self.loss = loss
+        self.name = loss.__class__.__name__.lower().replace("loss", "")
+
+    def calculate_difference(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """Calculate the loss.
+
+        Parameters
+        ----------
+        pred : torch.Tensor
+            Prediction tensor, shape (bs, ensemble, lat*lon, n_outputs)
+        target : torch.Tensor
+            Target tensor, shape (bs, ensemble, lat*lon, n_outputs)
+
+        Returns
+        -------
+        torch.Tensor
+            Loss
+        """
+        return self.loss(pred, target)
