5223d5e100038de9d2fd462d60cdbdbfd6e169f4
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-07-04 11:04:10 +0000

    test new block and chunk

---
+++ models/tests/layers/block/test_block_pointwise.py
@@ -0,0 +1,94 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import logging
+
+import torch
+from hypothesis import given
+from hypothesis import settings
+from hypothesis import strategies as st
+from torch import nn
+
+from anemoi.models.layers.block import MLP
+from anemoi.models.layers.block import GraphConvProcessorBlock
+from anemoi.models.layers.block import PointWiseMLPProcessorBlock
+from anemoi.models.layers.conv import GraphConv
+from anemoi.models.layers.utils import load_layer_kernels
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestPointWiseMLPProcessorBlock:
+    @given(
+        hidden_dim=st.integers(min_value=1, max_value=100),
+        num_channels=st.integers(min_value=16, max_value=64),
+        activation=st.sampled_from(
+            [
+                "torch.nn.ReLU",
+                "torch.nn.GELU",
+            ]
+        ),
+        dropout_p=st.floats(min_value=0.0, max_value=1.0),
+    )
+    @settings(max_examples=10)
+    def test_init(self, hidden_dim, num_channels, activation, dropout_p):
+        layer_kernels = load_layer_kernels({"Activation": {"_target_": activation}})
+
+        block = PointWiseMLPProcessorBlock(
+            num_channels=num_channels,
+            hidden_dim=hidden_dim,
+            dropout_p=dropout_p,
+            layer_kernels=layer_kernels,
+        )
+        assert isinstance(block, PointWiseMLPProcessorBlock)
+
+        assert isinstance(block.mlp, nn.Sequential)
+
+    @given(
+        hidden_dim=st.integers(min_value=1, max_value=100),
+        num_channels=st.integers(min_value=16, max_value=64),
+        activation=st.sampled_from(
+            [
+                "torch.nn.ReLU",
+                "torch.nn.GELU",
+                "anemoi.models.layers.activations.GLU",
+                "anemoi.models.layers.activations.SwiGLU",
+            ]
+        ),
+        shapes=st.lists(st.integers(min_value=1, max_value=10), min_size=3, max_size=3),
+        batch_size=st.integers(min_value=1, max_value=40),
+        dropout_p=st.floats(min_value=0.0, max_value=1.0),
+    )
+    @settings(max_examples=10)
+    def test_forward_output(
+        self,
+        hidden_dim,
+        num_channels,
+        activation,
+        shapes,
+        batch_size,
+        dropout_p,
+    ):
+        kwargs = dict()
+        if "GLU" in activation:
+            kwargs["dim"] = hidden_dim
+        layer_kernels = load_layer_kernels({"Activation": {"_target_": activation, **kwargs}})
+
+        block = PointWiseMLPProcessorBlock(
+            num_channels=num_channels,
+            hidden_dim=hidden_dim,
+            dropout_p=dropout_p,
+            layer_kernels=layer_kernels,
+        )
+
+        x = torch.randn((batch_size, num_channels))  # .to(torch.float16, non_blocking=True)
+        output = block.forward(x, shapes, batch_size)
+        assert isinstance(output, torch.Tensor)
+        assert output.shape == (batch_size, num_channels)
---
+++ models/tests/layers/chunk/test_chunk_pointwise.py
@@ -0,0 +1,53 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import pytest
+
+from anemoi.models.layers.block import PointWiseMLPProcessorBlock
+from anemoi.models.layers.chunk import PointWiseMLPProcessorChunk
+from anemoi.models.layers.utils import load_layer_kernels
+
+
+class TestPointWiseMLPProcessorChunk:
+    @pytest.fixture
+    def init(self):
+        num_channels = 512
+        num_layers = 3
+        mlp_hidden_ratio: int = 4
+        dropout_p: float = 0.1
+        layer_kernels = load_layer_kernels()
+
+        return (
+            num_channels,
+            num_layers,
+            layer_kernels,
+            mlp_hidden_ratio,
+            dropout_p,
+        )
+
+    @pytest.fixture
+    def processor_chunk(self, init):
+        (
+            num_channels,
+            num_layers,
+            layer_kernels,
+            mlp_hidden_ratio,
+            dropout_p,
+        ) = init
+        return PointWiseMLPProcessorChunk(
+            num_channels=num_channels,
+            num_layers=num_layers,
+            layer_kernels=layer_kernels,
+            mlp_hidden_ratio=mlp_hidden_ratio,
+            dropout_p=dropout_p,
+        )
+
+    def test_all_blocks(self, processor_chunk):
+        assert all(isinstance(block, PointWiseMLPProcessorBlock) for block in processor_chunk.blocks)
