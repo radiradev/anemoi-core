7ebe248bf23f5bcf01779fc9ea1a2c2039f6e7fe
Mathias Schreiner <matschreiner@gmail.com>
2025-07-30 14:54:23 +0200

    add residual tests

--- models/tests/layers/test_residual.py
+++ models/tests/layers/test_residual.py
@@ -4,8 +4,6 @@ from torch_geometric.data import HeteroData
 
 from anemoi.models.layers.residual import TruncationMapper
 
-#  from anemoi.models.layers.residual import hans
-
 
 @pytest.fixture
 def up_down_graph():
@@ -19,14 +17,17 @@ def up_down_graph():
     graph["fine"].x = x_fine
     graph["coarse"].x = x_coarse
     graph["coarse", "to", "fine"].edge_index = edge_index_up
+    graph["coarse", "to", "fine"].edge_attr = torch.tensor([1.0, 2.0])
+
     graph["fine", "to", "coarse"].edge_index = edge_index_down
+    graph["fine", "to", "coarse"].edge_attr = torch.tensor([1.0, 2.0])
 
     return graph
 
 
 @pytest.fixture
 def flat_data():
-    x = torch.randn(3, 2, 5)  # batch, node, features
+    x = torch.randn(11, 7, 5, 2, 3)  # batch, dates, ensemble, grid, features
     return x
 
 
@@ -35,24 +36,29 @@ def edge_index():
     return torch.tensor([[0, 1, 1], [1, 0, 2]])
 
 
-#  def test_truncation_matrix_from_edge_index(edge_index):
-#      edge_index = torch.tensor([[0, 1, 1], [1, 0, 2]])
-#      edge_attribute = torch.tensor([1.0, 2.0, 3.0])
-#      num_source_nodes = 2
-#      num_target_nodes = 3
-#
-#      A = TruncationMapper._create_truncation_matrix(edge_index, edge_attribute, num_source_nodes, num_target_nodes)
-#
-#      assert A.is_sparse
-#      assert A.shape == (2, 3)
-#      indices = A.indices()
-#      values = A.values()
-#
-#      expected_indices = torch.tensor([[0, 1, 1], [1, 0, 2]])
-#      expected_values = torch.tensor([1.0, 2.0, 3.0])
-#
-#      assert torch.equal(indices, expected_indices)
-#      assert torch.equal(values, expected_values)
+def test_truncation_matrix_from_edge_index(edge_index):
+    edge_index = torch.tensor([[0, 1, 1], [1, 0, 2]])
+    edge_attribute = torch.tensor([1.0, 2.0, 3.0])
+    num_source_nodes = 2
+    num_target_nodes = 3
+
+    A = TruncationMapper._create_sparse_projection_matrix(
+        edge_index,
+        edge_attribute,
+        num_source_nodes,
+        num_target_nodes,
+    )
+
+    assert A.is_sparse
+    assert A.shape == (2, 3)
+    indices = A.indices()
+    values = A.values()
+
+    expected_indices = torch.tensor([[0, 1, 1], [1, 0, 2]])
+    expected_values = torch.tensor([1.0, 2.0, 3.0])
+
+    assert torch.equal(indices, expected_indices)
+    assert torch.equal(values, expected_values)
 
 
 def test_truncation_mapper_init(up_down_graph):
@@ -61,13 +67,24 @@ def test_truncation_mapper_init(up_down_graph):
     assert hasattr(mapper, "A_down")
 
 
-def test_truncate_fields(up_down_graph, flat_data):
+def test_sparse_projection(up_down_graph):
     mapper = TruncationMapper(up_down_graph)
-    mapper._truncate_fields(flat_data, mapper.A_down)
-    #  assert hasattr(mapper, "A_up")
+    x = torch.randn(10, 2, 3)
+
+    x = mapper._sparse_projection(mapper.A_down, x)
+    assert x.shape == (10, 1, 3)
 
 
-#  def test_truncation_mapper_forward(up_down_graph, flat_data):
-#      mapper = TruncationMapper(up_down_graph)
-#      mapper(flat_data)
-#      #  up_down_graph["fine", "to", "coarse"]
+def test_truncate_fields(up_down_graph):
+    mapper = TruncationMapper(up_down_graph)
+    x = torch.randn(10, 2, 3)  # (batch*ensemble, grid, features)
+    x_truncated = mapper._truncate_fields(x)
+
+    assert x_truncated.shape == x.shape
+
+
+def test_forward(up_down_graph):
+    mapper = TruncationMapper(up_down_graph)
+    x = torch.randn(5, 2, 2, 2, 3)  # (batch, dates, ensemble, grid, features)
+    x_truncated = mapper.forward(x)
+    assert x_truncated.shape == (5, 2, 2, 3)  # (batch, ensemble, coarse_grid, features)
