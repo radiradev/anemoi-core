71da8f3f1c7ed6b40112443594ae651cec0ecd95
Vera <121622878+VeraChristina@users.noreply.github.com>
2025-08-08 09:48:44 +0100

    test: replace longtest markers with slow (#468)
    ## Description
    This PR replaces the longtest markers and --longtests cli option for
    pytest with slow markers and --slow.
    
    ## What problem does this change solve?
    Make markers and cli for slow tests consistent across the anemoi
    packages.
    
    ## What issue or task does this change relate to?
    https://github.com/ecmwf/anemoi-core/issues/257
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)

--- training/tests/integration/aicon/test_cicd_aicon_04_icon-dream_medium.py
+++ training/tests/integration/aicon/test_cicd_aicon_04_icon-dream_medium.py
@@ -99,7 +99,7 @@ def test_config_validation_aicon(aicon_config_with_tmp_dir: DictConfig) -> None:
     BaseSchema(**aicon_config_with_tmp_dir)
 
 
-@pytest.mark.longtests
+@pytest.mark.slow
 @typechecked
 def test_aicon_metadata(aicon_config_with_grid: DictConfig) -> None:
     """Test for presence of metadata required for inference.
@@ -134,7 +134,7 @@ def test_aicon_metadata(aicon_config_with_grid: DictConfig) -> None:
     assert trainer.model.model.model.decoder.proc.num_chunks == aicon_config_with_grid.model.decoder.num_chunks
 
 
-@pytest.mark.longtests
+@pytest.mark.slow
 @typechecked
 def test_aicon_training(trained_aicon: tuple) -> None:
     trainer, initial_sum, final_sum = trained_aicon
