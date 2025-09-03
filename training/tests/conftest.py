b52d060dec7419de6b21a0865f9b6cd6f55f2a11
Mathias Schreiner <matschreiner@gmail.com>
2025-09-03 10:55:15 +0200

    remove addoption from conftest

--- training/tests/conftest.py
+++ training/tests/conftest.py
@@ -22,16 +22,6 @@ from anemoi.training.data.datamodule import AnemoiDatasetsDataModule
 pytest_plugins = "anemoi.utils.testing"
 
 
-def pytest_addoption(parser: pytest.Parser) -> None:
-    group = parser.getgroup("anemoi")
-    group.addoption("--cache", action="store_true", default=False, help="Enable caching of test datasets")
-
-
-@pytest.fixture(scope="session")
-def cache_enabled(request: pytest.FixtureRequest) -> bool:
-    return bool(request.config.getoption("--cache"))
-
-
 @pytest.fixture
 def config(request: SubRequest) -> DictConfig:
     overrides = request.param
