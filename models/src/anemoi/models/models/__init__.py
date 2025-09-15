0bb9ffa8814048761f223527cfaa9391ed720b5c
anaprietonem <ana.prietonemesio@ecmwf.int>
2025-09-15 15:21:23 +0000

    fix circular import

--- models/src/anemoi/models/models/__init__.py
+++ models/src/anemoi/models/models/__init__.py
@@ -8,14 +8,14 @@
 # nor does it submit to any jurisdiction.
 
 from .base import AnemoiGraphModelBase
+
+# isort: off - to avoid circular import error
+from .diffusion_encoder_processor_decoder import AnemoiDiffusionModelEncProcDec
+from .diffusion_encoder_processor_decoder import AnemoiDiffusionTendModelEncProcDec
 from .encoder_processor_decoder import AnemoiModelEncProcDec
 from .ens_encoder_processor_decoder import AnemoiEnsModelEncProcDec
 from .hierarchical import AnemoiModelEncProcDecHierarchical
 from .interpolator import AnemoiModelEncProcDecInterpolator
-
-# isort: off
-from .diffusion_encoder_processor_decoder import AnemoiDiffusionModelEncProcDec
-from .diffusion_encoder_processor_decoder import AnemoiDiffusionTendModelEncProcDec
 
 # isort: on
 
