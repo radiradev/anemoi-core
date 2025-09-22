7977a0b1bcb38e5a8f612c81971eed8b08511d97
Florian Pinault <Florian.Pinault@ecmwf.int>
2025-09-22 20:15:04 +0000

    fix toy model for downscaling

--- models/src/anemoi/models/models/downscaling.py
+++ models/src/anemoi/models/models/downscaling.py
@@ -7,6 +7,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+import torch
+
 from anemoi.models.models import AnemoiMultiModel
 
 
@@ -17,6 +19,17 @@ class AnemoiDownscalingModel(AnemoiMultiModel):
 class ToyAnemoiDownscalingModel(AnemoiMultiModel):
     name = "downscaling"
 
+    def build(self):
+        input_info = self.sample_static_info["input"]
+        target_info = self.sample_static_info["target"]
+
+        assert len(target_info) == 1, "This toy model only supports one target key"
+
+        self.num_input_channels = len(input_info["high_res"]["name_to_index"])
+        self.num_output_channels = len(target_info.first["name_to_index"])
+
+        self.linear = torch.nn.Linear(self.num_input_channels, self.num_output_channels)
+
     def forward(self, x, *args, **kwargs):
         print(f"----- Start of Forward pass of {self.__class__.__name__} -----")
         print(f"Ignoring args: {args}")
@@ -26,17 +39,18 @@ class ToyAnemoiDownscalingModel(AnemoiMultiModel):
         print(self.sample_static_info.to_str("sample_static_info"))
 
         print(x.to_str("input x"))
+        data = x["high_res"]["data"]
 
-        output = self.sample_static_info["target"].new_empty()
-        for k, value in self.sample_static_info["target"].items():
-            res = value.copy()
-            module = self.linear[k]  # get the linear layer for this key
-            data = x[k]["data"]  # get the input data for this key
-            data = einops.rearrange(data, "1 a b -> 1 b a")
-            res["data"] = module(data)  # apply the linear layer
-            output[k] = res  # save in output for this key
+        data = einops.rearrange(data, "b variables values -> b values variables")
+        pred = self.linear(data)
+        pred = einops.rearrange(pred, "b values variables -> b variables values")
 
-        print(output.to_str("output after linear"))
-        assert len(output), "ouput must not be empty"
-        print("❤️🆗----- End of Forward pass of AnemoiMultiModel -----")
-        return output
+        n_variables = len(self.sample_static_info["target"].first["name_to_index"])
+        assert pred.shape[1] == n_variables, f"Expected output shape {n_variables}, got {pred.shape[1]}"
+
+        print("❤️🆗----- End of Forward pass of ToyAnemoiDownscalingModel -----")
+        res = x.new_empty()
+        box = self.sample_static_info["target"]["high_res"].copy()
+        box["data"] = pred
+        res["high_res"] = box
+        return res
