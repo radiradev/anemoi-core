361e7139e703dec95839ed08c4de1b4033fb8c20
Cathal O'Brien <Cathal.OBrien@ecmwf.int>
2026-07-27 10:39:06 +0200

    fix(training): fix race condition in benchmark tests (#1267)
    ## Description
    sometimes the final benchmark test will fail, when process 0 can't find
    the files it needs to analyse the performance. that's bc rank 1 has
    already finished and deleted the files. this PR adds a barrier at the
    end of the test (outside the timer) to prevent this.
    
    also I renamed the dataloader benchmark so it runs after the other
    benchmarks, bc I got annoyed at waiting

--- training/tests/integration/test_benchmark.py
+++ training/tests/integration/test_benchmark.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 import psutil
 import pytest
+import torch.distributed as dist
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 from torch.cuda import empty_cache
@@ -60,7 +61,7 @@ def restore_base_seed(original_seed: str | None) -> None:
 
 @pytest.mark.multigpu
 @pytest.mark.slow
-def test_benchmark_dataloader(
+def test_benchmark_training_dataloader(
     benchmark_config: tuple[DictConfig, str],  # cfg, benchmarkTestCase,
 ) -> None:
     """Runs a benchmark for dataloader performance, testing MultiDataset batch sampling speed."""
@@ -156,3 +157,9 @@ def test_benchmark_training_cycle(
     store: str = f"ssh://{user}@{hostname}:{path}"
 
     benchmark(cfg, test_case, store)
+
+    # barrier to ensure all processes have completed before finishing the test
+    # otherwise process 0 will finish the final test before process 0
+    # has finished comparing the results against the benchmark server
+    # torch.dist is initialized in the benchmark function, so we can use it here
+    dist.barrier()
